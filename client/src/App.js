6a067132d412ed4117bb1841baaf3d2beba15536
Amber-Pittman <amber@amberpittman.com>
2019-12-13 20:54:27 -0500

    Extra stylings

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PlayerAPI from './Components/PlayerAPI/PlayerAPI';
 import DarkModeNavBar from "./Components/DarkMode/DarkModeNavBar";
+// import Styled from "styled-components";
 
+// const StyledDiv = Styled("div")`
+//   background-color: #f5f5f5;
+// `;
 
 function App() {
   return(
     <div className="App">
       
       <DarkModeNavBar />
-      <PlayerAPI />
+
+      {/* <StyledDiv> */}
+        <PlayerAPI />
+      {/* </StyledDiv> */}
     </div>
+    
   );
   
 }
